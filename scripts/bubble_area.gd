2b8632622e96c0342cd4d2274e727734ef7bbbe5
UmbraLux <shadowcard@gmx.de>
2025-01-24 23:24:10 +0100

    changed spawn behaviours rom bubble_area to respective spawner

--- scripts/bubble_area.gd
+++ scripts/bubble_area.gd
@@ -1,31 +1,33 @@
 extends Node2D
 
 @onready var score = 0
-@onready var bubble_scene = preload("res://scenes/bubble.tscn")
-@onready var timer
+#@onready var bubble_scene = preload("res://scenes/bubble.tscn")
+#@onready var timer
 var accum_time = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Start the timer to spawn bubbles every 2 seconds
+	$SpawnArea/BubbleSpawn/BubbleSpawn_1.unlockSpawner() # Start the timer to spawn bubbles every 2 seconds
 
 func _process(delta):
-	accum_time += delta
-	if accum_time>1:
-		spawn_bubble()
-		accum_time = 0
+	#accum_time += delta
+	#if accum_time>1:
+	#	spawn_bubble()
+	#	accum_time = 0
+	pass
 
 func spawn_bubble():
 	# Randomize the position where the bubble will spawn
 	#var random_position = Vector2(randf_range(0, get_viewport().size.x), randf_range(0, get_viewport().size.y))
-	var point = $SpawnArea/BubbleSpawn/BubbleSpawn_1
+	#var point = $SpawnArea/BubbleSpawn/BubbleSpawn_1
 	
-	var bubble_instance = bubble_scene.instantiate()
+	#var bubble_instance = bubble_scene.instantiate()
 	#if bubble_instance is PackedScene:
-	bubble_instance.position = point.position
-	$SpawnArea/Bubbles.add_child(bubble_instance)
+	#bubble_instance.position = point.position
+	#$SpawnArea/Bubbles.add_child(bubble_instance)
 	#else:
 	#	print("error no bubble")
+	pass
 
 func add_score(points):
 	score += points
---
+++ scripts/bubble_spawner.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+#unlocks
+var spawner_unlocked = false
+
+#vars
+var bubble_scene = preload("res://scenes/bubble.tscn")
+var accum_time = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if spawner_unlocked == true:
+		accum_time += delta
+		if accum_time>1:
+			spawn_bubble()
+			accum_time = 0
+
+func spawn_bubble():
+	var bubble_instance = bubble_scene.instantiate()
+	if bubble_instance != null:
+		bubble_instance.position = position
+		$"../../Bubbles".add_child(bubble_instance)
+	else:
+		print("error no bubble")
+
+func unlockSpawner() -> void:
+	spawner_unlocked = true
