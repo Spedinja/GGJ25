3303199e3c4bbfe110cfbbfd7618d2d5a499c7bf
UmbraLux <shadowcard@gmx.de>
2025-01-25 22:56:13 +0100

    added all buttons

--- scripts/shopUpgrades.gd
+++ scripts/shopUpgrades.gd
@@ -25,10 +25,10 @@ func _process(delta: float) -> void:
 
 func _on_btn_upgrade_1_pressed() -> void:
 	currentLvl = arrCurrLevel[0]
-	if currentLvl <= maxLvl:
+	if currentLvl < maxLvl:
 		print(currentLvl)
 		$"../../BubbleArea/SpawnArea/BubbleSpawn/BubbleSpawn_1".levelUp(currentLvl)
-		SignalManager.money_changed.emit((arrUpgradeCosts[0][arrCurrLevel[currentLvl]])*-1)
+		SignalManager.money_changed.emit((arrUpgradeCosts[0][currentLvl])*-1)
 		currentLvl = currentLvl+1
 		arrCurrLevel[0] = currentLvl
 
@@ -57,28 +57,53 @@ func _on_bubble_area_cash_changed(value: Variant) -> void:
 
 func _on_btn_upgrade_2_pressed() -> void:
 	currentLvl = arrCurrLevel[1]
-	if currentLvl <= maxLvl:
+	if currentLvl < maxLvl:
 		$"../../BubbleArea/SpawnArea/BubbleSpawn/BubbleSpawn_2".levelUp(currentLvl)
-		SignalManager.money_changed.emit((arrUpgradeCosts[1][arrCurrLevel[currentLvl]])*-1)
+		SignalManager.money_changed.emit((arrUpgradeCosts[1][currentLvl])*-1)
 		currentLvl = currentLvl +1
 		arrCurrLevel[1] = currentLvl
 
 
 func _on_btn_upgrade_3_pressed() -> void:
-	pass # Replace with function body.
+	currentLvl = arrCurrLevel[2]
+	if currentLvl < maxLvl:
+		$"../../BubbleArea/SpawnArea/BubbleSpawn/BubbleSpawn_3".levelUp(currentLvl)
+		SignalManager.money_changed.emit((arrUpgradeCosts[2][currentLvl])*-1)
+		currentLvl = currentLvl +1
+		arrCurrLevel[2] = currentLvl
 
 
 func _on_btn_upgrade_4_pressed() -> void:
-	pass # Replace with function body.
+	currentLvl = arrCurrLevel[3]
+	if currentLvl < maxLvl:
+		$"../../BubbleArea/SpawnArea/BubbleSpawn/BubbleSpawn_4".levelUp(currentLvl)
+		SignalManager.money_changed.emit((arrUpgradeCosts[3][currentLvl])*-1)
+		currentLvl = currentLvl +1
+		arrCurrLevel[3] = currentLvl
 
 
 func _on_btn_upgrade_5_pressed() -> void:
-	pass # Replace with function body.
+	currentLvl = arrCurrLevel[4]
+	if currentLvl < maxLvl:
+		$"../../BubbleArea/SpawnArea/BubbleSpawn/BubbleSpawn_5".levelUp(currentLvl)
+		SignalManager.money_changed.emit((arrUpgradeCosts[4][currentLvl])*-1)
+		currentLvl = currentLvl +1
+		arrCurrLevel[4] = currentLvl
 
 
 func _on_btn_upgrade_6_pressed() -> void:
-	pass # Replace with function body.
+	currentLvl = arrCurrLevel[5]
+	if currentLvl < maxLvl:
+		$"../../BubbleArea/SpawnArea/BubbleSpawn/BubbleSpawn_6".levelUp(currentLvl)
+		SignalManager.money_changed.emit((arrUpgradeCosts[5][currentLvl])*-1)
+		currentLvl = currentLvl +1
+		arrCurrLevel[5] = currentLvl
 
 
 func _on_btn_upgrade_7_pressed() -> void:
-	pass # Replace with function body.
+	currentLvl = arrCurrLevel[6]
+	if currentLvl < maxLvl:
+		$"../../BubbleArea/SpawnArea/BubbleSpawn/BubbleSpawn_7".levelUp(currentLvl)
+		SignalManager.money_changed.emit((arrUpgradeCosts[6][currentLvl])*-1)
+		currentLvl = currentLvl +1
+		arrCurrLevel[6] = currentLvl
