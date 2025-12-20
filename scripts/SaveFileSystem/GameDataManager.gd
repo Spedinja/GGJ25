c474c8f4aca3aa22d37b18c14f72a15d200c441f
UmbraLux <shadowcard@gmx.de>
2025-12-20 19:15:12 +0100

    added test button for save/load added palyer_area to new group for money test -> money save and load in bubble area -> needs to be setup for the rest as the function already works the way its intended now
    adjusted code for save/load; just needs to be added to all other things

--- scripts/SaveFileSystem/GameDataManager.gd
+++ scripts/SaveFileSystem/GameDataManager.gd
@@ -5,38 +5,31 @@ extends Node
 #important to use this as save path, as all other wont work especially on android
 const save_file_path:= "user://savegame.save"
 
-
-#var save_data = {
-#  "gold": currGold,
-#  "stations_upgrade_LvL": station_levels,
-#}
-
-#func initialize_defaults(station_count: int):
-	#score = 0
-	#station_levels = []
-	#for i in station_count:
-		#station_levels.append(0)
+var score: int = 0
+var station_levels: Array[int] = []
 
 # Note: This can be called from anywhere inside the tree. This function is
 # path independent.
 # Go through everything in the persist category and ask them to return a
 # dict of relevant variables.
 func save_game(): #durch alle persistables, classnames and extract relevant data
-	var save_file = FileAccess.open("user://savegame.save", FileAccess.WRITE)
+		
+	var save_file = FileAccess.open(save_file_path, FileAccess.WRITE)
 	var save_nodes = get_tree().get_nodes_in_group("Persistables")
 	for node in save_nodes:
 		# Check the node is an instanced scene so it can be instanced again during load.
-		if node.scene_file_path.is_empty():
-			print("persistent node '%s' is not an instanced scene, skipped" % node.name)
+		#if node.scene_file_path.is_empty():
+		if node.get_path().is_empty():
+			print("no path '%s' skipped" % node.name)
 			continue
-
+			
 		# Check the node has a save function.
-		if !node.has_method("save"):
+		if !node.has_method("save_state"):
 			print("persistent node '%s' is missing a save() function, skipped" % node.name)
 			continue
 
 		# Call the node's save function.
-		var node_data = node.call("save")
+		var node_data = node.call("save_state")
 
 		# JSON provides a static method to serialized JSON string.
 		var json_string = JSON.stringify(node_data)
@@ -47,7 +40,7 @@ func save_game(): #durch alle persistables, classnames and extract relevant data
 # Note: This can be called from anywhere inside the tree. This function
 # is path independent.
 func load_game():
-	if not FileAccess.file_exists("user://savegame.save"):
+	if not FileAccess.file_exists(save_file_path):
 		return # Error! We don't have a save to load.
 
 	# We need to revert the game state so we're not cloning objects
@@ -60,7 +53,7 @@ func load_game():
 
 	# Load the file line by line and process that dictionary to restore
 	# the object it represents.
-	var save_file = FileAccess.open("user://savegame.save", FileAccess.READ)
+	var save_file = FileAccess.open(save_file_path, FileAccess.READ)
 	while save_file.get_position() < save_file.get_length():
 		var json_string = save_file.get_line()
 
@@ -77,12 +70,17 @@ func load_game():
 		var node_data = json.data
 
 		# Firstly, we need to create the object and add it to the tree and set its position.
-		var new_object = load(node_data["filename"]).instantiate()
-		get_node(node_data["parent"]).add_child(new_object)
-		new_object.position = Vector2(node_data["pos_x"], node_data["pos_y"])
-
+		#var new_object = load(node_data["filename"]).instantiate()
+		#get_node(node_data["parent"]).add_child(new_object)
+		#new_object.position = Vector2(node_data["pos_x"], node_data["pos_y"])
+		
+		var object_to_load = get_node(node_data["filename"])
+		
 		# Now we set the remaining variables.
 		for i in node_data.keys():
 			if i == "filename" or i == "parent" or i == "pos_x" or i == "pos_y":
 				continue
-			new_object.set(i, node_data[i])
+			object_to_load.set(i, node_data[i])
+			
+			if !object_to_load.has_method("load_state"):
+				object_to_load.call("load_state")
