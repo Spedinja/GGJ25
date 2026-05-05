f7e2a037c8a94504074be0581479dc77058ed9bf
UmbraLux <shadowcard@gmx.de>
2026-05-05 15:19:34 +0200

    removed save code from tutorial -> exception in save and load

--- scripts/tutorial/tutorial_1.gd
+++ scripts/tutorial/tutorial_1.gd
@@ -13,17 +13,3 @@ func on_first_unlock():
 	var tutorial2 = load("res://scenes/tutorial/tutorial_2.tscn") as PackedScene
 	parent.add_child(tutorial2.instantiate())
 	queue_free()
-
-func save_state() -> Dictionary:
-	var save_dict = { 
-		"filename" : self.get_path(), 
-		"parent" : get_parent().get_path(), 
-		"tutorial_completed" : SignalManager.tutorial_completed, 
-		} 
-	return save_dict
-
-func load_state_withDict(data: Dictionary) -> void:
-	if data.get("tutorial_completed",true):
-		SignalManager.tutorial_completed = true
-	else:
-		SignalManager.tutorial_completed = false
